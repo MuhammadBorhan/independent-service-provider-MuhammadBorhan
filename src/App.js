2bddcba7bf0dc7e70218157df493857687570945
Muhammad Borhan <0317371981@student.wub.edu.bd>
2022-04-17 00:32:29 +0600

    all npm install done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>borhan</h1>
+      <h1 className='text-primary'>borhan</h1>
     </div>
   );
 }

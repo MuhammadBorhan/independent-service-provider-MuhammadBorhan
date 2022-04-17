83aa3e1adeb972a46d17a4d6af56df81846315c2
Muhammad Borhan <0317371981@student.wub.edu.bd>
2022-04-17 16:04:32 +0600

    signin with email and password dnoe

--- src/LoginPage/Login/Login.js
+++ src/LoginPage/Login/Login.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { Link, useNavigate } from 'react-router-dom';
 import auth from '../../Firebase/Firebase.init';
 import './Login.css';
 
 const Login = () => {
     const [signInWithGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth);
+    const [signInWithEmailAndPassword, user, loading, error] = useSignInWithEmailAndPassword(auth);
     const navigate = useNavigate();
     if (googleUser) {
         navigate('/checkout')
     }
+
+    if (user) {
+        navigate('/checkout')
+    }
+
+    const handleSignIn = event => {
+        event.preventDefault();
+
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        signInWithEmailAndPassword(email, password)
+    }
     return (
         <div className='container login-form'>
             <h3 className='text-center mt-3 text-success fw-bold'>Please Login</h3>
-            <Form className='w-50 mx-auto p-2 border my-3'>
+            <Form onSubmit={handleSignIn} className='w-50 mx-auto p-2 border my-3'>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
                     <Form.Control type="email" name="email" placeholder="Enter email" required />
@@ -23,6 +36,10 @@ const Login = () => {
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
                     <Form.Control type="password" name="password" placeholder="Password" required />
+                    <p className='text-center text-danger fw-bold fs-5 mt-1'>{error?.message}</p>
+                    {
+                        loading && <p className='text-center text-success fw-bold fs-5'>Loading...</p>
+                    }
                 </Form.Group>
 
                 <Button className='mx-auto d-block fw-bold' variant="primary" type="submit">
